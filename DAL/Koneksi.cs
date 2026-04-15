d7b275340266d25c33c0305d523658bbd4a75738
Kysohee18 <hanashahky@gmail.com>
2026-04-15 22:53:38 +0700

    add private string koneksi

--- DAL/Koneksi.cs
+++ DAL/Koneksi.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace Ucp_pabd_lab.DAL
 {
     internal class Koneksi
     {
+        private string stringKoneksi = "Data Source=DESKTOP-2Q8C9J6;Initial Catalog=DBLabSekolah;Integrated Security=True";
+        
+        public SqlConnection GetConn()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = stringKoneksi;
+            return conn;
+        }
     }
 }
