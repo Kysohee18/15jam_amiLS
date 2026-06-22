eb9b5d917917aa34277e6893bf0e948262816ffe
Kysohee18 <hanashahky@gmail.com>
2026-06-22 20:26:32 +0700

    Add UC_LogPenjaga, UC_Peminjaman, UC_Pengembalian forms
    Added three new form classes (UC_LogPenjaga, UC_Peminjaman, UC_Pengembalian) with their designer files. Updated the project file to include these forms, each implemented as a partial class inheriting from Form with default initialization and designer code.

---
+++ UI/UC_LogPenjaga.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucp_pabd_lab.UI
+{
+    public partial class UC_LogPenjaga : Form
+    {
+        public UC_LogPenjaga()
+        {
+            InitializeComponent();
+        }
+    }
+}
